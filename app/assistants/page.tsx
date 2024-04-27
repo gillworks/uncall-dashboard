ad010fb05b6d15286e174dfa8b30c397104fa9db
Michael Gill <66725656+gillworks@users.noreply.github.com>
2024-04-27 12:45:54 -0400

    create new assistant dialog

---
+++ app/assistants/page.tsx
@@ -0,0 +1,28 @@
+import { getUsers, getAssistants } from '@/lib/db';
+import { UsersTable } from '../users-table';
+import { AssistantsTable } from '../assistants-table';
+import { Search } from '../search';
+import { AddAssistantDialog } from '@/components/assistants/AddAssistantDialog';
+
+export default async function IndexPage({
+  searchParams
+}: {
+  searchParams: { q: string; offset: string };
+}) {
+  const search = searchParams.q ?? '';
+  const offset = searchParams.offset ?? 0;
+  const { assistants, newOffset } = await getAssistants(search, Number(offset));
+
+  return (
+    <main className="flex flex-1 flex-col p-4 md:p-6">
+      <div className="flex items-center justify-between mb-8">
+        <h1 className="font-semibold text-lg md:text-2xl">Assistants</h1>
+        <AddAssistantDialog />
+      </div>
+      <div className="w-full mb-4">
+        <Search value={searchParams.q} />
+      </div>
+      <AssistantsTable assistants={assistants} offset={newOffset} />
+    </main>
+  );
+}
